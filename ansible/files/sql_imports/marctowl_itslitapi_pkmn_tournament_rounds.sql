5ad0b8b69b2be7dcfa63a2b79d705ba66e3199e4
Marc Towler <marc@marctowler.co.uk>
2026-02-23 00:19:42 +0000

    Added sql files for deployment

--- ansible/files/sql_imports/marctowl_itslitapi_pkmn_tournament_rounds.sql
+++ ansible/files/sql_imports/marctowl_itslitapi_pkmn_tournament_rounds.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:aad1811724cf6dc4c672c9da52e0f5327d94a0f36ba784f8e23917daba48bc9a
-size 77628661
+oid sha256:d10ba265e26a85495892f90182172021a29fe28a977e7f78d9a8d738e6fa3ea6
+size 84876954
