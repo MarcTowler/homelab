6823fcf9fdc27c0abaa0aad3d0949bbc706a75ea
Marc Towler <marc@marctowler.co.uk>
2026-02-23 00:18:26 +0000

    Added sql files for deployment

--- ansible/files/sql_imports/marctowl_itslitapi_pkmn_decklists.sql
+++ ansible/files/sql_imports/marctowl_itslitapi_pkmn_decklists.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5f771b0394f1be0a58cccbff2420cbfab60476286ff5ef5e3118d2abeb118719
-size 198678385
+oid sha256:d1f1687f101efbde1e7f8b38448053ea68852ff7767f03cfdc77c138308c4d3b
+size 81859311
---
+++ ansible/files/sql_imports/marctowl_itslitapi_pkmn_tournament_rounds2.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:46de75c9f19a631eaa42b739980139d130ac9597db765412e8017ffc37a223f3
+size 87680733
